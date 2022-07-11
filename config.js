8c4d7b49f1c0aa60bf6177e4de0a33804b29bcd5
Katherine Law <katherine.law2@gmail.com>
2022-07-11 11:05:42 -0400

    updating gitignore files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '679501072415900',
-        clientSecret: 'eb5ef77888d1151bfb2896ef0eecc83d'
-    }
-}
